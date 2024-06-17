b135accda7836c1ed3aa9118676411a90365269b
villavant <jorge.villavicencio10@gmail.com>
2024-06-17 12:28:24 -0500

    Configuration static

--- HolaDepa/settings.py
+++ HolaDepa/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ['.pythonanywhere.com']
+ALLOWED_HOSTS = ["127.0.0.1",'.pythonanywhere.com']
 
